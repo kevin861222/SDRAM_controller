b22ad1132edd79e36e411e3b67e50cae8e456d43
Wang Yu <79128379+kevin861222@users.noreply.github.com>
2024-01-04 13:38:14 +0800

    Add files via upload

---
+++ testbench/counter_la_mm/counter_la_mm.elf-isr.s
@@ -0,0 +1,491 @@
+	.file	"isr.c"
+	.option nopic
+	.attribute arch, "rv32i2p0"
+	.attribute unaligned_access, 0
+	.attribute stack_align, 16
+	.text
+.Ltext0:
+	.cfi_sections	.debug_frame
+	.file 0 "/home/a605/soclab/labD-sdram/testbench/counter_la_mm" "../../firmware/isr.c"
+	.align	2
+	.type	flush_cpu_icache, @function
+flush_cpu_icache:
+.LFB21:
+	.file 1 "../../firmware/system.h"
+	.loc 1 15 1
+	.cfi_startproc
+	addi	sp,sp,-16
+	.cfi_def_cfa_offset 16
+	sw	s0,12(sp)
+	.cfi_offset 8, -4
+	addi	s0,sp,16
+	.cfi_def_cfa 8, 0
+	.loc 1 26 1
+	nop
+	lw	s0,12(sp)
+	.cfi_restore 8
+	.cfi_def_cfa 2, 16
+	addi	sp,sp,16
+	.cfi_def_cfa_offset 0
+	jr	ra
+	.cfi_endproc
+.LFE21:
+	.size	flush_cpu_icache, .-flush_cpu_icache
+	.align	2
+	.type	flush_cpu_dcache, @function
+flush_cpu_dcache:
+.LFB22:
+	.loc 1 29 1
+	.cfi_startproc
+	addi	sp,sp,-16
+	.cfi_def_cfa_offset 16
+	sw	s0,12(sp)
+	.cfi_offset 8, -4
+	addi	s0,sp,16
+	.cfi_def_cfa 8, 0
+	.loc 1 33 1
+	nop
+	lw	s0,12(sp)
+	.cfi_restore 8
+	.cfi_def_cfa 2, 16
+	addi	sp,sp,16
+	.cfi_def_cfa_offset 0
+	jr	ra
+	.cfi_endproc
+.LFE22:
+	.size	flush_cpu_dcache, .-flush_cpu_dcache
+	.align	2
+	.type	irq_setmask, @function
+irq_setmask:
+.LFB319:
+	.file 2 "../../firmware/irq_vex.h"
+	.loc 2 30 1
+	.cfi_startproc
+	addi	sp,sp,-32
+	.cfi_def_cfa_offset 32
+	sw	s0,28(sp)
+	.cfi_offset 8, -4
+	addi	s0,sp,32
+	.cfi_def_cfa 8, 0
+	sw	a0,-20(s0)
+	.loc 2 31 2
+	lw	a5,-20(s0)
+ #APP
+# 31 "../../firmware/irq_vex.h" 1
+	csrw 3008, a5
+# 0 "" 2
+	.loc 2 32 1
+ #NO_APP
+	nop
+	lw	s0,28(sp)
+	.cfi_restore 8
+	.cfi_def_cfa 2, 32
+	addi	sp,sp,32
+	.cfi_def_cfa_offset 0
+	jr	ra
+	.cfi_endproc
+.LFE319:
+	.size	irq_setmask, .-irq_setmask
+	.globl	flag
+	.section	.sbss,"aw",@nobits
+	.align	1
+	.type	flag, @object
+	.size	flag, 2
+flag:
+	.zero	2
+	.text
+	.align	2
+	.globl	isr
+	.type	isr, @function
+isr:
+.LFB321:
+	.file 3 "../../firmware/isr.c"
+	.loc 3 17 1
+	.cfi_startproc
+	addi	sp,sp,-16
+	.cfi_def_cfa_offset 16
+	sw	ra,12(sp)
+	sw	s0,8(sp)
+	.cfi_offset 1, -4
+	.cfi_offset 8, -8
+	addi	s0,sp,16
+	.cfi_def_cfa 8, 0
+	.loc 3 22 2
+	li	a0,0
+	call	irq_setmask
+	.loc 3 32 5
+	li	a5,-268423168
+	addi	a5,a5,56
+	.loc 3 32 58
+	li	a4,10
+	sw	a4,0(a5)
+	.loc 3 33 5
+	li	a5,-268423168
+	addi	a5,a5,60
+	.loc 3 33 59
+	li	a4,131072
+	sw	a4,0(a5)
+	.loc 3 34 9
+	lui	a5,%hi(flag)
+	li	a4,1
+	sh	a4,%lo(flag)(a5)
+	.loc 3 42 5
+	nop
+	.loc 3 48 1
+	lw	ra,12(sp)
+	.cfi_restore 1
+	lw	s0,8(sp)
+	.cfi_restore 8
+	.cfi_def_cfa 2, 16
+	addi	sp,sp,16
+	.cfi_def_cfa_offset 0
+	jr	ra
+	.cfi_endproc
+.LFE321:
+	.size	isr, .-isr
+.Letext0:
+	.file 4 "/opt/riscv/lib/gcc/riscv32-unknown-elf/12.1.0/include/stdint-gcc.h"
+	.section	.debug_info,"",@progbits
+.Ldebug_info0:
+	.4byte	0xe9
+	.2byte	0x5
+	.byte	0x1
+	.byte	0x4
+	.4byte	.Ldebug_abbrev0
+	.byte	0x4
+	.4byte	.LASF15
+	.byte	0x1d
+	.4byte	.LASF0
+	.4byte	.LASF1
+	.4byte	.Ltext0
+	.4byte	.Letext0-.Ltext0
+	.4byte	.Ldebug_line0
+	.byte	0x1
+	.byte	0x1
+	.byte	0x6
+	.4byte	.LASF2
+	.byte	0x1
+	.byte	0x2
+	.byte	0x5
+	.4byte	.LASF3
+	.byte	0x1
+	.byte	0x4
+	.byte	0x5
+	.4byte	.LASF4
+	.byte	0x1
+	.byte	0x8
+	.byte	0x5
+	.4byte	.LASF5
+	.byte	0x1
+	.byte	0x1
+	.byte	0x8
+	.4byte	.LASF6
+	.byte	0x2
+	.4byte	.LASF8
+	.byte	0x31
+	.byte	0x1c
+	.4byte	0x54
+	.byte	0x1
+	.byte	0x2
+	.byte	0x7
+	.4byte	.LASF7
+	.byte	0x2
+	.4byte	.LASF9
+	.byte	0x34
+	.byte	0x1b
+	.4byte	0x66
+	.byte	0x1
+	.byte	0x4
+	.byte	0x7
+	.4byte	.LASF10
+	.byte	0x1
+	.byte	0x8
+	.byte	0x7
+	.4byte	.LASF11
+	.byte	0x5
+	.byte	0x4
+	.byte	0x5
+	.string	"int"
+	.byte	0x1
+	.byte	0x4
+	.byte	0x7
+	.4byte	.LASF12
+	.byte	0x6
+	.4byte	.LASF16
+	.byte	0x3
+	.byte	0xe
+	.byte	0xa
+	.4byte	0x49
+	.byte	0x5
+	.byte	0x3
+	.4byte	flag
+	.byte	0x7
+	.string	"isr"
+	.byte	0x3
+	.byte	0x10
+	.byte	0x6
+	.4byte	.LFB321
+	.4byte	.LFE321-.LFB321
+	.byte	0x1
+	.byte	0x9c
+	.byte	0x8
+	.4byte	.LASF17
+	.byte	0x2
+	.byte	0x1d
+	.byte	0x14
+	.4byte	.LFB319
+	.4byte	.LFE319-.LFB319
+	.byte	0x1
+	.byte	0x9c
+	.4byte	0xcc
+	.byte	0x9
+	.4byte	.LASF18
+	.byte	0x2
+	.byte	0x1d
+	.byte	0x2d
+	.4byte	0x7b
+	.byte	0x2
+	.byte	0x91
+	.byte	0x6c
+	.byte	0
+	.byte	0x3
+	.4byte	.LASF13
+	.byte	0x1c
+	.4byte	.LFB22
+	.4byte	.LFE22-.LFB22
+	.byte	0x1
+	.byte	0x9c
+	.byte	0x3
+	.4byte	.LASF14
+	.byte	0xe
+	.4byte	.LFB21
+	.4byte	.LFE21-.LFB21
+	.byte	0x1
+	.byte	0x9c
+	.byte	0
+	.section	.debug_abbrev,"",@progbits
+.Ldebug_abbrev0:
+	.byte	0x1
+	.byte	0x24
+	.byte	0
+	.byte	0xb
+	.byte	0xb
+	.byte	0x3e
+	.byte	0xb
+	.byte	0x3
+	.byte	0xe
+	.byte	0
+	.byte	0
+	.byte	0x2
+	.byte	0x16
+	.byte	0
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0x21
+	.byte	0x4
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x49
+	.byte	0x13
+	.byte	0
+	.byte	0
+	.byte	0x3
+	.byte	0x2e
+	.byte	0
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0x21
+	.byte	0x1
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0x21
+	.byte	0x25
+	.byte	0x27
+	.byte	0x19
+	.byte	0x11
+	.byte	0x1
+	.byte	0x12
+	.byte	0x6
+	.byte	0x40
+	.byte	0x18
+	.byte	0x7a
+	.byte	0x19
+	.byte	0
+	.byte	0
+	.byte	0x4
+	.byte	0x11
+	.byte	0x1
+	.byte	0x25
+	.byte	0xe
+	.byte	0x13
+	.byte	0xb
+	.byte	0x3
+	.byte	0x1f
+	.byte	0x1b
+	.byte	0x1f
+	.byte	0x11
+	.byte	0x1
+	.byte	0x12
+	.byte	0x6
+	.byte	0x10
+	.byte	0x17
+	.byte	0
+	.byte	0
+	.byte	0x5
+	.byte	0x24
+	.byte	0
+	.byte	0xb
+	.byte	0xb
+	.byte	0x3e
+	.byte	0xb
+	.byte	0x3
+	.byte	0x8
+	.byte	0
+	.byte	0
+	.byte	0x6
+	.byte	0x34
+	.byte	0
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x49
+	.byte	0x13
+	.byte	0x3f
+	.byte	0x19
+	.byte	0x2
+	.byte	0x18
+	.byte	0
+	.byte	0
+	.byte	0x7
+	.byte	0x2e
+	.byte	0
+	.byte	0x3f
+	.byte	0x19
+	.byte	0x3
+	.byte	0x8
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x27
+	.byte	0x19
+	.byte	0x11
+	.byte	0x1
+	.byte	0x12
+	.byte	0x6
+	.byte	0x40
+	.byte	0x18
+	.byte	0x7c
+	.byte	0x19
+	.byte	0
+	.byte	0
+	.byte	0x8
+	.byte	0x2e
+	.byte	0x1
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x27
+	.byte	0x19
+	.byte	0x11
+	.byte	0x1
+	.byte	0x12
+	.byte	0x6
+	.byte	0x40
+	.byte	0x18
+	.byte	0x7a
+	.byte	0x19
+	.byte	0x1
+	.byte	0x13
+	.byte	0
+	.byte	0
+	.byte	0x9
+	.byte	0x5
+	.byte	0
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x49
+	.byte	0x13
+	.byte	0x2
+	.byte	0x18
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_aranges,"",@progbits
+	.4byte	0x1c
+	.2byte	0x2
+	.4byte	.Ldebug_info0
+	.byte	0x4
+	.byte	0
+	.2byte	0
+	.2byte	0
+	.4byte	.Ltext0
+	.4byte	.Letext0-.Ltext0
+	.4byte	0
+	.4byte	0
+	.section	.debug_line,"",@progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",@progbits,1
+.LASF13:
+	.string	"flush_cpu_dcache"
+.LASF6:
+	.string	"unsigned char"
+.LASF10:
+	.string	"long unsigned int"
+.LASF7:
+	.string	"short unsigned int"
+.LASF15:
+	.string	"GNU C17 12.1.0 -mabi=ilp32 -mtune=rocket -misa-spec=2.2 -march=rv32i -g -ffreestanding"
+.LASF17:
+	.string	"irq_setmask"
+.LASF16:
+	.string	"flag"
+.LASF12:
+	.string	"unsigned int"
+.LASF11:
+	.string	"long long unsigned int"
+.LASF14:
+	.string	"flush_cpu_icache"
+.LASF5:
+	.string	"long long int"
+.LASF18:
+	.string	"mask"
+.LASF3:
+	.string	"short int"
+.LASF8:
+	.string	"uint16_t"
+.LASF9:
+	.string	"uint32_t"
+.LASF4:
+	.string	"long int"
+.LASF2:
+	.string	"signed char"
+	.section	.debug_line_str,"MS",@progbits,1
+.LASF0:
+	.string	"../../firmware/isr.c"
+.LASF1:
+	.string	"/home/a605/soclab/labD-sdram/testbench/counter_la_mm"
+	.ident	"GCC: (g1ea978e3066) 12.1.0"
---
+++ testbench/counter_la_mm/counter_la_mm.elf-matmul.i
@@ -0,0 +1,43 @@
+# 0 "matmul.c"
+# 1 "/home/a605/soclab/labD-sdram/testbench/counter_la_mm//"
+# 0 "<built-in>"
+# 0 "<command-line>"
+# 1 "matmul.c"
+# 1 "matmul.h" 1
+
+
+
+
+ int A[4*4] = {
+   0, 1, 2, 3,
+   0, 1, 2, 3,
+   0, 1, 2, 3,
+   0, 1, 2, 3,
+ };
+ int B[4*4] = {
+  1,   2,  3,  4,
+  5,   6,  7,  8,
+  9,  10, 11, 12,
+  13, 14, 15, 16,
+ };
+ int result[4*4];
+# 2 "matmul.c" 2
+
+int* __attribute__ ( ( section ( ".mprjram" ) ) ) matmul()
+{
+ int i=0;
+ int j;
+ int k;
+ int sum;
+ int kk;
+ unsigned int count = 0;
+ for (i=0; i<4; i++){
+  for (j=0; j<4; j++){
+   sum = 0;
+   for(k = 0;k<4;k++)
+    sum += A[(i*4) + k] * B[(k*4) + j];
+   result[(i*4) + j] = sum;
+  }
+ }
+ return result;
+}
